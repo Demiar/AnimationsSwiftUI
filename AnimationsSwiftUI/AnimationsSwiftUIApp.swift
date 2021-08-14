c6e128d11cf3c568003ddbd8aa987d7cb67de35e
Demiar <alexey174@gmail.com>
2021-08-14 12:09:08 +0300

    Initial Commit

---
+++ AnimationsSwiftUI/AnimationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsSwiftUIApp.swift
+//  AnimationsSwiftUI
+//
+//  Created by Алексей on 14.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
