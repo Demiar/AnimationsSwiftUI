73a1087e6540bbad0152de93e184adf8672d6f78
Demiar <alexey174@gmail.com>
2021-08-15 15:34:37 +0300

    Done

--- AnimationsSwiftUI/ContentView.swift
+++ AnimationsSwiftUI/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetail = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 60 / 255, green: 100 / 255, blue: 126 / 255, opacity: 1)
+                .ignoresSafeArea()
+            VStack{
+                ZStack{
+                    Image("410814-PDWTNO-879")
+                        .resizable()
+                        .frame(width: 500, height: 500)
+                        Image("ufo")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .offset(y: -165)
+                            .rotationEffect(.degrees(showDetail ? 0 : 360))
+                            .animation(.linear.speed(0.1).repeatForever(autoreverses: false))
+                            .onAppear(perform: {
+                                self.showDetail.toggle()
+                            })
+                }
+
+            }
+        }
     }
 }
 
